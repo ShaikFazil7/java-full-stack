45e083373313b9a6cef5a5058dc6dba616bda8f8
ShaikFazil7 <24hr1a30a0@mtieat.org>
2026-06-11 14:10:28 +0530

    HTML.CSS

---
+++ HTML.CSS/script.js
@@ -0,0 +1,107 @@
+/*let name="Shaik Fazil";
+var age=20;
+const height="1.72";
+console.log(age);
+console.log(age+2);
+console.log(age-1);
+console.log(age*2);
+console.log(age/2);
+console.log(age%2);
+age+=3;
+console.log(age);
+age-=3;
+console.log(age);
+age*=2;
+console.log(age);
+age/=2;
+console.log(age);
+age%=2;
+console.log(age);
+let age1=19;
+console.log(age<age1);
+console.log(age>age1);
+console.log(age>=age1);
+console.log(age<=age1);
+console.log(age==age1);
+console.log(age!=age1);*/
+
+
+/*let food1="Pizza";
+let brealfast="idli";
+let food2="biriyani";
+let food3="dosa";
+let food4="chapati";
+let food5="pasta";
+if(food1=brealfast)
+{
+    console.log("its nice to have a biryani for breakfast");
+}
+else
+{
+    console.log("its not good to have a idli for breakfast");
+} */
+
+
+/*let amount=4700;
+let rupees500=0;
+let rupees200=0;
+let rupees100=0;
+if(amount>=500)
+{
+    while(amount>=500);
+    rupees500++;
+    amount=amount-500;
+}
+if(amount>=200)
+{
+    while(amount>=200);
+    rupees200++;
+    amount=amount-200;
+}
+if(amount>=100)
+{
+    while(amount>=100);
+    rupees100++;
+    amount=amount-100;
+}
+console.log(" 500 rupess notes are:"+rupees500);
+console.log(" 200 rupess notes are:"+rupees200);
+console.log(" 100 rupess notes are:"+rupees100);*/
+
+
+
+/*for(let i=1;i<=30;i++)
+{
+    console.log("day "+i+" completed");
+}*/
+
+
+/*let i=1;
+while(i<=30)
+    {
+        console.log("day "+i+" completed");
+        i++;
+    } */
+
+
+/*let name="Shaik Fazil";
+let password="1111";
+let name_1="fazil";
+let pin="2222";
+let name_2="mushraf";
+let pin_2="mushraf@7";
+if(name_1="fazil")
+    console.log(name_1);
+if(pin="2222")
+{
+    console.log("loged in successfull");
+}
+else{
+    console.log("loged in failed");
+} */
+
+
+function wash(vessels){
+console.log("we have to wash"+vessels+"vessels");
+}
+dishwash(2);
