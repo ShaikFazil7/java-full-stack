88afd331222dc7fdbf94f4c7d1e13b94d7e7e2b1
ShaikFazil7 <24hr1a30a0@mtieat.org>
2026-06-12 14:45:00 +0530

    HTML.CSS

---
+++ HTML.CSS/Manipulation.js
@@ -0,0 +1,4 @@
+let value1 = document.getElementById("wish");
+value1.innerHTML="<h1>happy birthday arshil</h>";
+value1.style.fontSize="50px";
+value1.style.fontStyle="italic";
---
+++ HTML.CSS/input.js
@@ -0,0 +1,29 @@
+function greet_afternoon()
+{
+    let text= document.getElementById("greet");
+    text.innerText="Good Afternoon 🤩";
+}
+function sunrise()
+{
+    let light=document.getElementById("sunrise");
+    light.src="C:\\Users\\shaik fazil\\Pictures\\Screenshots\\sunrise.jpeg";
+}
+function sunset()
+{
+    let light=document.getElementById("sunrise");
+    light.src="C:\\Users\\shaik fazil\\Pictures\\Screenshots\\sunset.jpeg";
+    const img = document.getElementById("sunrise");
+
+img.width = 300;
+img.height = 200;
+}
+function change_color()
+{
+    let color = document.getElementById("antony");
+    color.style.backgroundColor="pink";
+    let txt1 = document.getElementById("Data"); 
+}
+function change_image()
+{
+
+}
